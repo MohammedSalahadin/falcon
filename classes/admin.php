ad49369373617f08649b471b9d9ef2e57791c002
MohammedSalahadin <mohammed.salahadinf18@komar.edu.iq>
2022-02-26 11:09:16 +0300

    API login

--- classes/admin.php
+++ classes/admin.php
@@ -63,9 +63,7 @@ class Admin{
             //user have been registerd 
             $this->userName = $userName;
             
-            
         } else { echo "not";}
-        
         
     }
 
