569ede05e35b0c511748ce4dba136bb6c4f2e718
MohammedSalahadin <mohammed.salahadinf18@komar.edu.iq>
2022-02-28 19:22:06 +0300

    adding few uncomplete methods!

--- classes/system.php
+++ classes/system.php
@@ -40,14 +40,97 @@ class System{
     public $customer_MaintinanceSupervisor = array();
     //System main elements
     public $devices = array();                  //array of Multiple devices objects of the system
-    public $properties = array(); 
-    public $groups = array();
+    public $properties = array();               //array of the properites of the system
+    public $groups = array();                   // array of groups object
 
 
     public function __construct(){
 
     }
 
+    public function addGeneral($localURL, $hideHomePageMenuBar, $returnUrlOnLogout, $contactCompanyName, $dispachPhoneNumber, 
+    $cityStateZip, $contactEmail, $contactPhoneNumber, $handheldPhotoTimestampText, $includeArrivals_DeparturesInDAR,
+    $homePageMessage, $mobileDeviceLoginMessage, $propertyFindExampleText, $externalUrlLinks){
+        $id = 0;
+        return $id;
+    }
+
+    public function updateGeneral($localURL, $hideHomePageMenuBar, $returnUrlOnLogout, $contactCompanyName, $dispachPhoneNumber, 
+    $cityStateZip, $contactEmail, $contactPhoneNumber, $handheldPhotoTimestampText, $includeArrivals_DeparturesInDAR,
+    $homePageMessage, $mobileDeviceLoginMessage, $propertyFindExampleText, $externalUrlLinks){
+        $id = 0;
+        return $id;
+    }
+    //used to view data on front-end
+    public function getGeneral($localURL, $hideHomePageMenuBar, $returnUrlOnLogout, $contactCompanyName, $dispachPhoneNumber, 
+    $cityStateZip, $contactEmail, $contactPhoneNumber, $handheldPhotoTimestampText, $includeArrivals_DeparturesInDAR,
+    $homePageMessage, $mobileDeviceLoginMessage, $propertyFindExampleText, $externalUrlLinks){
+        $id = 0;
+        return $id;
+    }
+
+    public function addNotifications($resendNotificationAlertForUnacknowledgedIssuesPriority1,$resendNotificationAlertForUnacknowledgedIssuesPriority2,
+    $resendNotificationAlertForUnacknowledgedIssuesPriority3){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+
+    public function updateNotifications($resendNotificationAlertForUnacknowledgedIssuesPriority1,$resendNotificationAlertForUnacknowledgedIssuesPriority2,
+    $resendNotificationAlertForUnacknowledgedIssuesPriority3){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    //used to view data on front-end
+    public function getNotifications($resendNotificationAlertForUnacknowledgedIssuesPriority1,$resendNotificationAlertForUnacknowledgedIssuesPriority2,
+    $resendNotificationAlertForUnacknowledgedIssuesPriority3){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    public function addEmails($fromEmailAddressNewOrder,$fromEmailAddressNewIssue,$fromEmailAddressAppeals,$SystemEmailNotification){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    public function updateEmails($fromEmailAddressNewOrder,$fromEmailAddressNewIssue,$fromEmailAddressAppeals,$SystemEmailNotification){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    //used to view data on front-end
+    public function getEmails($fromEmailAddressNewOrder,$fromEmailAddressNewIssue,$fromEmailAddressAppeals,$SystemEmailNotification){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    
+    public function addLogos($mainPageLogo, $reportHeaderLogo){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    //get's data fron front-end
+    public function updateLogos($mainPageLogo, $reportHeaderLogo){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+    //used to view data on front-end
+    public function getLogos($mainPageLogo, $reportHeaderLogo){
+        $id = 0;
+        $query = "";
+        return $id;
+    }
+
+
+
+    public function createSystem(){
+        $query = "INSERT INTO `falcon`.`systems` (`general_id`, `devices_id`, `logos_id`, `notification_id`, `email_id`, `domainName`) VALUES ('1', '1', '1', '1', '1', 'test.falcontrac.com');";
+
+
+    }
 
 
 
