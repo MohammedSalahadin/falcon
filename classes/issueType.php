23aee0c7fdf1179f5e5ed4ca77d0e858e58cf908
Zhiar43 <50279850+Zhiar43@users.noreply.github.com>
2022-03-11 16:26:57 +0300

    chnaged the Function location

--- classes/issueType.php
+++ classes/issueType.php
@@ -42,8 +42,51 @@ class IssueTypes{
         return 0;
     }
 
-    public function create(){
-        return 0;
+    public function createIssueType($issueTypeName,$description,$issueFee,$issueLevel,$issueType,$active,$dispatch,$handheld,$webUsers,$autoClose,$checkPointOnly,$addTo)
+    {
+        try {
+            $dbConn = new db();
+            $conn = $dbConn->getConnection();
+            $conn->begin_transaction(); 
+
+            $query = "INSERT INTO `falcon`.`issue_types` (`issueTypeName`, `issueDescription`, `issueFee`, `issueLevel`, `issue_type`, `isActiveIssue`, `displayForDispach`, `displayOnHandheld`, `displayForWebUsers`, `autoCloseIssue`, `restrictToCheckpointOnly`) 
+            VALUES ('$issueTypeName','$description','$issueFee','$issueLevel','$issueType','$active','$dispatch','$handheld','$webUsers','$autoClose','$checkPointOnly');" ; 
+
+                if ($conn->query($query)) { // issueType has been Created
+                    echo $this->id."<br>";
+                    $this->name = $issueTypeName;
+                    echo $this->name."<br>";
+
+                    switch ($addTo) {
+                        case 'current':
+                            $query1 = "INSERT INTO `falcon`.`property_has_issue_types` (`property_id`, `issue_types_id`) 
+                            VALUES ('$this->id', '$conn->insert_id'); ";
+                                if($conn->query($query1)){ // added the most recent added issueType to the current property
+                                    echo " added the most recent added issueType to the current property ";
+                                }
+                            break;
+                        case 'allproperties':
+                            $query2 = "INSERT INTO `falcon`.`property_group_has_issue_types` (`property_group_id`, `issue_type_id`) 
+                            VALUES ('1', '$conn->insert_id'); ";
+                                if($conn->query($query2)){ // added the most recent added issueType to all properties
+                                    echo " added the most recent added issueType to all properties ";
+                                }
+                            break;
+                        default:
+                            echo "wrong command: $addTo, use 'current' or 'allproperties'";
+                            break;
+                    }
+
+                    
+                }
+           $conn->commit();
+           $conn->close();
+
+        } catch (\Throwable $th) {
+            $conn->rollback();
+            $conn->close();
+            return false;
+        }
     }
     
     public function generate(){
